fac42cc1855b70655e6375bd61239562960a98c3
Camilo Hernandez <camilohi.1196@gmail.com>
2024-12-05 15:15:04 -0500

    H1
    Movimiento en titulo, y en imagen

---
+++ static/js/titulo.js
@@ -0,0 +1,17 @@
+// Importar el archivo JavaScript de typed.js
+var script = document.createElement('script');
+script.src = 'https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js';
+
+// Esperar a que se cargue typed.js antes de continuar
+script.onload = function() {
+  // Inicializar Typed para el título
+  var typed = new Typed(".auto-type", {
+    strings: ["Energia Renovable", "100% Limpia", "La Energia del Futuro"],
+    cursorChar: '',
+    startDelay: 0,
+    tySpeed: 1000,
+    backSpeed: 100,
+    loop: true
+  });
+};
+document.head.appendChild(script);
