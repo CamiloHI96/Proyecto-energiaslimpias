26fa3018218d35b1e21cb11120d7fdcd37f6c3a8
Camilo Hernandez <camilohi.1196@gmail.com>
2024-12-06 13:28:30 -0500

    Cambios
    Cambios minimos en la pagina

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import csv
 from waitress import serve
 
 app = Flask(__name__)
 app.secret_key = 'camilo123'
 
+def limpiar_encabezados(encabezados):
+    return [encabezado.strip() for encabezado in encabezados]
+
 def cargar_datos_renovables(ruta_csv):
-    datos=[]
+    datos = []
     try:
         with open(ruta_csv, mode='r', encoding='utf-8') as archivo_csv:
             lector = csv.DictReader(archivo_csv)
+            print(f"Encabezados del archivo CSV: {lector.fieldnames}")  # Imprime los encabezados
             for fila in lector:
-                datos.append({'entity': fila['Entity'],'code': fila['Code'],'year': int(fila['Year']),'renewables': float(fila['Renewables (%equivalent primary energy)'])})
+                datos.append({
+                    'entity': fila['Entity'],
+                    'code': fila['Code'],
+                    'year': int(fila['Year']),
+                    'renewables': float(fila['Renewables (% equivalent primary energy)'])  # Nombre ajustado
+                })
     except Exception as e:
         print(f"Error al leer el archivo CSV: {e}")
     return datos
@@ -22,7 +31,7 @@ datos_renovables = cargar_datos_renovables(RUTA_CSV)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     porcentaje_renovable = None
-    error = None 
+    error = None
 
     if request.method == 'POST':
         try:
@@ -30,15 +39,21 @@ def index():
             if consumo_total <= 0:
                 error = 'El consumo total debe ser un valor positivo'
             else:
-                producto_total_renovable = sum(energia ['renewable'] for energia in datos_renovables)
+                producto_total_renovable = sum(energia['renewables'] for energia in datos_renovables)
                 if producto_total_renovable >= consumo_total:
-                    porcentaje_renovable = (consumo_total / producto_total_renovable)*100
+                    porcentaje_renovable = (consumo_total / producto_total_renovable) * 100
                 else:
                     porcentaje_renovable = 100
+
+            session['porcentaje_renovable'] = porcentaje_renovable
+            session['error'] = error
         except ValueError:
             error = 'Por favor ingrese un valor valido para el consumo total'
-    
-    return render_template('index.html', porcentaje_renovable = porcentaje_renovable, error=error)
+
+    porcentaje_renovable = session.get('porcentaje_renovable', None)
+    error = session.get('error', None)
+
+    return render_template('index.html', porcentaje_renovable=porcentaje_renovable, error=error)
 
 if __name__ == '__main__':
     #server para subir a Render
