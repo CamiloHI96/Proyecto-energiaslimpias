2e6e1df705b642312f2dfb3ad67689555d949026
Camilo Hernandez <camilohi.1196@gmail.com>
2024-12-11 12:08:37 -0500

    Cambios Esteticos #3

--- static/js/titulo.js
+++ static/js/titulo.js
@@ -6,12 +6,13 @@ script.src = 'https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js';
 script.onload = function() {
   // Inicializar Typed para el título
   var typed = new Typed(".auto-type", {
-    strings: ["Energia Renovable", "100% Limpia", "La Energia del Futuro"],
-    cursorChar: ' /',
-    startDelay: 1000,
-    tySpeed: 1000,
-    backSpeed: 100,
-    loop: true
-  });
+    strings: ["Energía Renovable", "100% Limpia", "La Energía del Futuro"],
+    cursorChar: '|',             // Cambia el cursor si prefieres algo más visible
+    startDelay: 500,             // Reduce el tiempo de espera inicial
+    typeSpeed: 50,               // Ajusta velocidad de tipeo
+    backSpeed: 30,               // Ajusta velocidad de borrado
+    smartBackspace: true,        // Evita escribir texto ya existente
+    loop: true                   // Animación infinita
+  });  
 };
 document.head.appendChild(script);
